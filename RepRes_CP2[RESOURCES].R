b2e016b5824b93c502aee36582299e1d6c1c9ac3
ASonCourse <sander.schrieken@specialtouch.nl>
2017-10-30 13:32:45 +0100

    Added scriptfile listing / loading required resources.

---
+++ RepRes_CP2[RESOURCES].R
@@ -0,0 +1,6 @@
+# List of resources that will have to be installed / loaded for the code in the
+# R Markdown file to run (knitr) and produce the desired HTML file.
+
+install.packages("dplyr")
+library(dplyr)
+
