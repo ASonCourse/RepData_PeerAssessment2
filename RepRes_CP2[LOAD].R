a98026d296d4c5d90db3f97d8eeca776a1c76df5
ASonCourse <sander.schrieken@specialtouch.nl>
2017-10-30 10:26:56 +0100

    Changed LOAD script; added source URL configured unzipping.

--- RepRes_CP2[LOAD].R
+++ RepRes_CP2[LOAD].R
@@ -1,30 +1,25 @@
-# This file / script describes the steps involved in loading the data into R. The
+# This file / script describes the steps involved in loading the StormData into R. The
 # code is minimally commented, since the .Rmd file contains a thorough description.
 
 # First, let's fix the date we run this script / download the data:
 Download_Date <- Sys.Date()
 
 # Next let's download the data from the internet:
-source_url <- "https://raw.githubusercontent.com/ASonCourse/RepData_PeerAssessment1/master/activity.zip"
+source_url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 
-download.file(source_url, destfile = "activity.zip", 
+download.file(source_url, destfile = "StormData.csv.bz2", 
               method = "curl")
 
-# Unzip downloaded file (unzipped file -> "activity.csv"):
-unzip("activity.zip", exdir = ".")
+# Unzip downloaded file (unzipped file -> "StormData.csv"):
+unzip("StormData.csv.bz2", exdir = ".")
 
 # Importing .csv file in R:
-activity <- read.csv("activity.csv", stringsAsFactors = FALSE)
+StormData <- read.csv("StormData.csv", stringsAsFactors = FALSE)
 
 # Confirm the data was imported properly:
-dim(activity)
-# There should be 17.568 observations — and there are exactly that many.
-# So the import is complete.
-str(activity)
-min(activity$date)
-max(activity$date)
-summary(activity)
+dim(StormData)
+
 
 # How many NA's?
-mean(is.na(activity$steps))
+
 
