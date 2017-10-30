e841911e27c1ea467dc6d153acc241dea382179c
ASonCourse <sander.schrieken@specialtouch.nl>
2017-10-30 10:46:52 +0100

    Removed code for unzipping the .csv.bz2 file; read.csv() can read in the compressed file...

--- RepRes_CP2[LOAD].R
+++ RepRes_CP2[LOAD].R
@@ -10,11 +10,19 @@ source_url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.
 download.file(source_url, destfile = "StormData.csv.bz2", 
               method = "curl")
 
-# Unzip downloaded file (unzipped file -> "StormData.csv"):
-unzip("StormData.csv.bz2", exdir = ".")
+# # Unzipping of downloaded file ("StormData.csv.bz2") is not necessary! It can be
+# read in directly with read.csv(). Maybe we should use read_csv() instead, because
+# the file is quite large and we might want to experiment a bit first with just a
+# couple of lines / observations before we import the whole file...
 
-# Reading .csv file into R:
-StormData <- read.csv("StormData.csv", stringsAsFactors = FALSE)
+# May be we should also consider a faster option because it takes minutes to get the
+# data into R with read.csv(). fread()?
+
+# Also caching should be looked at, because this is going to be an issue when we start
+# writing our R Markdown file.
+
+# Reading .csv.bz2 file into R:
+StormData <- read.csv("StormData.csv.bz2", stringsAsFactors = FALSE)
 
 # Confirm the data was imported properly:
 dim(StormData)
